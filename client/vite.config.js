a4710e857860717f33e627e3478a068a7ec16d1c
Adithya Nagaraja <adibamboost@gmail.com>
2024-12-07 22:21:05 +0530

    Changed Vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://api:3000', // Use Docker service name
+        changeOrigin: true,
         secure: false,
       },
     },
   },
   plugins: [react()],
-})
+});
